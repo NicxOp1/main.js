46d4d670ba6827e85460d701fae492cc41919510
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-09-30 00:26:53 -0300

    Alfa

---
+++ main.js
@@ -0,0 +1,26 @@
+let miNombre
+miNombre = "Nicolas"
+let miApellido
+miApellido = "Lopez"
+let miEdad 
+miEdad = 22
+let miMascota
+miMascota = "Mona" 
+let edadMascota
+edadMascota = 7
+
+Variables = [miNombre,miApellido,miEdad,miMascota,edadMascota]
+console.log(Variables)
+
+let nombreCompleto = miNombre + miApellido
+console.log(nombreCompleto)
+ let textoPresentacion
+ textoPresentacion=(" Hola, mi nombre es "+miNombre+" y mi apellido "+miApellido+", tengo "+miEdad+" ,vivo en Argentina en la provincia de Buenos Aires y tengo una perra llamada "+miMascota+", que tiene "+edadMascota+" años")
+ console.log(textoPresentacion)
+
+
+ let sumaEdades = miEdad + edadMascota
+ let restaEdades = miEdad - edadMascota
+ let productoEdades = miEdad * edadMascota
+ let divisionEdades = miEdad / edadMascota 
+miNombre = prompt=("Nombre del hijo")
